379f0ff0b1f665bfe9286b540592626236694c89
Ramzi Hidan <ramzihaidan9@gmail.com>
2025-02-10 10:54:05 +0300

    finishing search functionality

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,8 +1,31 @@
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import ExerciseCard from './ExerciseCard'
 
-const Exercises = () => {
+const Exercises = ({exercises, setExercises, bodyPart}) => {
+    console.log(exercises)
   return (
-    <div>Exercises</div>
+    <Box
+    id="exercises" 
+    sx={{
+        mt:{
+            lg:'110px'
+        }
+    }}
+    mt='50px'
+    p='20px'
+    >
+        <Typography variant='h3' mb='46px' >
+            Showing Results
+        </Typography>
+        <Stack direction='row' sx={{ gap:{ lg:'110px',xs:'50px'} }} 
+        flexWrap='wrap' justifyContent='center' >
+            { exercises.map((exercise,index)=>(
+                <ExerciseCard key={index}
+                exercise={exercise} />
+            )) }
+        </Stack>
+    </Box>
   )
 }
 
