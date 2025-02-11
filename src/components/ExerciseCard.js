efdcd818cbcb263aa86155647a9450888fe8d955
Ramzi Hidan <ramzihaidan9@gmail.com>
2025-02-11 11:40:22 +0300

    adding detail page

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,4 +1,5 @@
-import { Link, Stack, Button, Typography } from '@mui/material'
+import { Stack, Button, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 const ExerciseCard = ({exercise}) => {
